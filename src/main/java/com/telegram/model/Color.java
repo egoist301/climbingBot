4130bd85aca69036c1cad2d86a849095e01517dc
Yauheni Filippovich <filippovich.z@mail.ru>
2022-01-28 18:04:33 +0300

    Add decoder to enum

--- src/main/java/com/telegram/model/Color.java
+++ src/main/java/com/telegram/model/Color.java
@@ -1,10 +1,21 @@
 package com.telegram.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.util.stream.Stream;
+
 public enum Color {
   RED,
   GREEN,
   WHITE,
   YELLOW,
   BLUE,
-  PURPLE
+  PURPLE;
+
+  @JsonCreator
+  public static Color decode(final String color) {
+    return Stream.of(Color.values())
+        .filter(targetEnum -> targetEnum.equals(Color.valueOf(color.toUpperCase()))).findFirst()
+        .orElse(null);
+  }
 }
